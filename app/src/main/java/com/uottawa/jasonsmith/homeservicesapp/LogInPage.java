e6ecf51f6a83aecdd5264bec29d4c93ba22fc200
Jason Smith <jasonasmith03@gmail.com>
2018-10-25 19:29:32 -0400

    Display username and role in welcome page

--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/LogInPage.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/LogInPage.java
@@ -4,33 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.WrapperListAdapter;
 
 public class LogInPage extends AppCompatActivity {
 
     Admin admin = new Admin();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+
     }
 
     public void register(View view) {
         //Application Context and Activity
         Intent intent = new Intent(getApplicationContext(), RegistrationInfo.class);
-        startActivityForResult (intent,0);
+        startActivityForResult(intent, 0);
     }
 
     public void logIn(View view) {
         //Application Context and Activity
-        EditText usernameInput = (EditText)findViewById(R.id.usernameText);
+        EditText usernameInput = (EditText) findViewById(R.id.usernameText);
         String usernameContent = usernameInput.getText().toString();
-        EditText passwordInput = (EditText)findViewById(R.id.PasswordText);
+        EditText passwordInput = (EditText) findViewById(R.id.PasswordText);
         String passwordContent = passwordInput.getText().toString();
 
-        if(usernameContent.equals(admin.getUsername()) && passwordContent.equals(admin.getPassword())){
-            Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
-            startActivityForResult (intent,0);
+        if (usernameContent.equals(admin.getUsername()) && passwordContent.equals(admin.getPassword())) {
+            //Application Context and Activity
+            Intent intent = new Intent(this, WelcomeScreen.class);
+            intent.putExtra("username", admin.getUsername());
+            intent.putExtra("role", "Admin");
+            startActivity(intent);
         }
 
     }
--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/WelcomeScreen.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/WelcomeScreen.java
@@ -1,13 +1,28 @@
 package com.uottawa.jasonsmith.homeservicesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    Admin admin = new Admin();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        Intent usernameIntent = getIntent();
+
+        String message1 = usernameIntent.getStringExtra("username");
+        String message2 = usernameIntent.getStringExtra("role");
+
+        TextView tview1 = (TextView)findViewById(R.id.displayUsername);
+        TextView tview2 = (TextView)findViewById(R.id.displayRole);
+
+        tview1.setText(message1);
+        tview2.setText(message2);
     }
+
 }
