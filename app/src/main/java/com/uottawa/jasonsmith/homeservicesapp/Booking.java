875640eb17e0ce62304c1f2566fed0a4316cece9
Joseph Peters <jpete012@uottawa.ca>
2018-10-25 16:10:51 -0400

    Created User class and Booking class.

--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/Booking.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/Booking.java
@@ -1,8 +1,32 @@
 package com.uottawa.jasonsmith.homeservicesapp;
 
+import java.sql.Timestamp;
+
 public class Booking {
 
-    public Booking(){
+    private User user;
+    private Service service;
+    private Timestamp date;
 
+    //User instance assigned to an instance of booking will have specified what service they want (service) and when (date)
+    //Once service has occurred, User instance may give a rating to the service (setRating), stored in the Booking instance.
+    public Booking(Timestamp date, User user, Service service){
+        this.date = date;
+        this.user = user;
+        this.service = service;
+    }
+
+    public void setServiceRating(int rating) {
+        service.addRating(rating);
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+    public User getUser() {
+        return user;
+    }
+    public Service getService() {
+        return service;
     }
 }
--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/User.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/User.java
@@ -1,15 +1,31 @@
 package com.uottawa.jasonsmith.homeservicesapp;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class User extends Person {
 
+    //In addition to storing the Person class information, a User instance also stores an address and a list of the current bookings
+    //ATTENTION----------------bookings to be the current bookings or all bookings (history of bookings
     private String address;
-    private ArrayList<Booking>;
+    private ArrayList<Booking> bookings;
+    private String username;
+    private String email;
+    private String password;
 
     public User(String username, String email, String password) {
-        super(username, email, password);
+            super(username, email, password);
     }
 
-    public
+    public void bookService(Date date, Service service) {
+        bookings.add(new Booking(date, this, service));
+    }
+
+    public void rateService(int rating, Booking booking){
+        booking.setServiceRating(rating);
+    }
+    public Service searchServices(String){
+        //PUT CODE HERE
+        return new Service("TEST", 1.0);
+    }
 }
