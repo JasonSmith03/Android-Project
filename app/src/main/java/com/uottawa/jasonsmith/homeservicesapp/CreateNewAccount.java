164c99a8e8415b8d55130a4d20be8c7d53bc9fcb
Joseph Peters <jpete012@uottawa.ca>
2018-10-25 12:54:34 -0400

    Linked SignUp button to Welcome page

--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/CreateNewAccount.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/CreateNewAccount.java
@@ -1,7 +1,9 @@
 package com.uottawa.jasonsmith.homeservicesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateNewAccount extends AppCompatActivity {
 
@@ -10,4 +12,8 @@ public class CreateNewAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_account);
     }
+    public void signUp(View view) {
+        //Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
+        startActivityForResult (intent,0);
 }
