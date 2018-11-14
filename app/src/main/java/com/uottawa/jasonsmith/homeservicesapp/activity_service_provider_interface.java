e5da04ac1d84243feb3b31ef4271557bcaffbbec
Jason Smith <jasonasmith03@gmail.com>
2018-11-14 18:42:28 -0500

    Created service provider interface

---
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/activity_service_provider_interface.java
@@ -0,0 +1,13 @@
+package com.uottawa.jasonsmith.homeservicesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_service_provider_interface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_provider_interface);
+    }
+}
