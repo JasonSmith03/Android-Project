b79476e605e4b1d58d8856f842216a3fd0840d1a
btrose98 <rose.bradley9@gmail.com>
2018-11-14 18:45:26 -0500

    added getters and setters for all fields of ServiceProvider

--- app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/ServiceProvider.java
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/ServiceProvider.java
@@ -11,7 +11,7 @@ public class ServiceProvider extends Person {
     private String address;
     private String email;
     private String phoneNumber;
-    private String nameOfCompany;
+    private String companyName;
     private boolean licensed;
 
 
@@ -25,6 +25,55 @@ public class ServiceProvider extends Person {
 
     public void associateWithService(){
         //TODO code this method
+    }
 
+    //getters and setters needed in the case that a service provider wants to change their information.
+    public String getUsername(){
+        return this.username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCompanyName(){
+        return this.companyName;
+    }
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+
+    public boolean getLicensed(){
+        return licensed;
+    }
+    public void setLicensed(boolean licensed){
+        this.licensed = licensed;
     }
 }
