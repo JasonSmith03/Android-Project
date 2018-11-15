e92b868a71614c7644b6c8095ff911ef3461eb3a
btrose98 <rose.bradley9@gmail.com>
2018-11-14 19:50:45 -0500

    ServiceProvider information page and service provider interface

---
+++ app/src/main/java/com/uottawa/jasonsmith/homeservicesapp/activity_SP_information.java
@@ -0,0 +1,13 @@
+package com.uottawa.jasonsmith.homeservicesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_SP_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__sp_information);
+    }
+}
